7f1003a004c4c7c343034745c26761a86e587841
Ignacio Hurtado <ignaciohurtado91@gmail.com>
2020-01-10 16:19:07 -0300

    .

--- src/Module.php
+++ src/Module.php
@@ -3,9 +3,6 @@
 namespace lucidprogrammer\simplesamlphp;
 
 use yii\base\Module as BaseModule;
-use lucidprogrammer\simplesamlphp\components\Saml;
-use lucidprogrammer\simplesamlphp\components\SamlSettings;
-
 
 class Module extends BaseModule {
     public $controllerNamespace = 'lucidprogrammer\simplesamlphp\controllers';
