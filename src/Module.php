718a2b8bfc079101aa8d7c207083faa7f8a6afe3
Ignacio Hurtado <ignaciohurtado91@gmail.com>
2020-01-10 17:28:33 -0300

    Revert "."
    This reverts commit 7f1003a004c4c7c343034745c26761a86e587841.

--- src/Module.php
+++ src/Module.php
@@ -3,6 +3,9 @@
 namespace lucidprogrammer\simplesamlphp;
 
 use yii\base\Module as BaseModule;
+use lucidprogrammer\simplesamlphp\components\Saml;
+use lucidprogrammer\simplesamlphp\components\SamlSettings;
+
 
 class Module extends BaseModule {
     public $controllerNamespace = 'lucidprogrammer\simplesamlphp\controllers';
